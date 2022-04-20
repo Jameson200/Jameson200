715323738e17c19d922894a2ce148d8893efd71f
Kojdecka Barbora <koj0010@vsb.cz>
2022-04-20 15:42:59 +0200

    added SQLInjection.java

---
+++ SQLInjection.java
@@ -0,0 +1,39 @@
+package cz.vsb.ekf.koj0010;
+
+import java.util.Scanner;
+import java.sql.*;
+
+public class SQLInjection {
+    public static final Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.println("Zadejte osobní číslo: ");
+        String osCislo = sc.nextLine();
+        System.out.println("Zadejte heslo: ");
+        String heslo = sc.nextLine();
+        
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:64376;databaseName=ZkouskyData", "sa", "student");
+                 Statement stat = con.createStatement();
+                 ResultSet rs = stat.executeQuery(
+                         "select OsCislo, Prijmeni, Jmeno "
+                       + "from tblStudenti "
+                       + "where OsCislo='" + osCislo + "' and Heslo='" + heslo + "'")
+                    ) {
+            
+                if (rs.next()) {
+                    System.out.println(rs.getString("OsCislo") + ", " + rs.getString("Prijmeni") + " " + rs.getString("Jmeno"));
+                } else {
+                    System.out.println("Nesprávné osobní číslo nebo heslo");
+                }
+                
+            } catch (SQLException ex) {
+                System.out.println("Problém s databázi: " + ex.getMessage());
+            }
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Nebyl nalezen ovladač.");
+        }
+    }
+    
+}
